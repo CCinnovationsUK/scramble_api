fa384055df3de25e0fa495034137e1919b880ff7
Cameron C <cc.innovations.uk@gmail.com>
2021-01-18 18:01:48 +0000

    Initial commit

---
+++ src/main/java/ai/ccinnov/scramble/api/scrambleRecord.java
@@ -0,0 +1,43 @@
+package ai.ccinnov.scramble.api;
+
+public class scrambleRecord {
+    public scrambleRecord(){}
+
+    public scrambleRecord(
+            String alias,
+            String inbox){
+        super();
+        this.alias = alias;
+        this.inbox = inbox;
+    }
+
+    private String alias;
+    private String inbox;
+
+    @Override
+    public String toString(){
+        return "scrambleRecord [alias="
+                +alias+", inbox ="
+                +inbox+"]";
+    }
+
+    //Autogen Getters & Setters
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getInbox() {
+        return inbox;
+    }
+
+    public void setInbox(String inbox) {
+        this.inbox = inbox;
+    }
+
+
+
+}
